60e6c5acd389c7b3dc86bf21ef1a8d68ada0a617
Red Tuby <pe1rrr@amsat.org>
2020-12-12 09:02:34 +0100

    Tidied up and added documentation

--- browse.sh
+++ browse.sh
@@ -1,13 +1,63 @@
 #!/bin/bash 
 
+# Version 1.0 PE1RRR WEB Portal for Packet
+# Configuration
+# 
+# For simplicity, I use openbsd's inetd which is available for Debian/Raspbian with sudo apt install openbsd-inetd
+# 
+# Add the following line to /etc/inetd.conf
+# 
+# browse		stream	tcp	nowait	bpq		/full/path/to/your/packet-scriptlets//browse.sh client ax25
+# 
+# The word 'bpq' above refers to the userid that this process will run under.
+#
+# Add the following line to /etc/services and make a note of the port number you choose. Make sure the one you pick does
+# not exist and is within the range of ports available.
+#
+# browse		63004/tcp   # Browser
+# 
+# Enable inetd: sudo systemctl enable inetd
+#               sudo service inetd start
+#
+# In bpq32.cfg add the port specified above (63004) to the BPQ Telnet port list, CMDPORTS= 
+#
+# Note the port's position offset in the list as that will be referenced in the APPLICATION line next.
+# The first port in the CMDPORTS= line is position 0 (zero), the next would be 1, then 2 and so on.
+#
+# Locate your APPLICATION line definitions in bpq32.cfg and another one next in the sequence. Make
+# sure it has a unique <app number> between 1-32.
+#
+# Syntax: 
+# APPLICATION <app number 1-32>,<node command>,<node instruction>
+#
+# APPLICATION 25,WEB,C 10 HOST 1 S
+#
+# <node instruction> is where the command 'web' is told to use BPQ port 10 (the telnet port, yours may be different!)
+# HOST is a command to tell BPQ to look at the BPQ Telnet CMDPORTS list, and '1' is to pick offset position 1, that
+# in turn relsolves to TCP port 63004. The 'S' tells the node to return the user back to the node when they exit
+# the web portal instead of disconnecting them, it refers to the word 'Stay'.
+
+lynx="/usr/bin/lynx"  # sudo apt install lynx
+curl="/usr/bin/curl"  # sudo apt install curl
+logfile="/var/log/bpq-browser.log" # sudo touch /var/log/bpq-browser; sudo chmod bpq:bpq /var/log/bpq-browser
+
+# Logfile: Again the word 'bpq' above refers to the user and group that the inetd server starts the process under. It must be
+# the same to have permission to write to this logfile.
+
+# It is recommended to set up a proxy server locally to handle the requests from this script
+# it adds a level of control over which content can and cannot be requested, squid proxy is
+# utilized for this, but if you do not want to use it, comment the 2 lines out below.
+# I have set up my squid proxy to use alternate DNS servers of OpenDNS FamilyShield.
+
+myproxy="http://127.0.0.1:3128"
+export http_proxy=$myproxy
+
+
+# Further config is at the bottom of the file for customization of the menu options as well as welcome message.
+##### End of Config - Do not change anything below here.
 linkregex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 yesnoregex='^(Y|y)$'
 quitcmds='^(B|b|q|Q)$'
-lynx="/usr/bin/lynx"
-myproxy="http://127.0.0.1:3128"
-curl="/usr/bin/curl"
-logfile="/var/log/bpq-browser.log"
-
 
 set -e
 trap 'catch $? $LINENO' EXIT
@@ -25,12 +75,10 @@ then
 	read callsignin
 elif	[ "$1" == "ax25" ]
 then
-	# Connection came from a linbpq client on 'ax25' port which sends callsign on connect.
+	# Connection came from a linbpq client on 'ax25' port which by default sends callsign on connect.
 	read callsignin
 fi
 
-export http_proxy=$myproxy
-#export https_proxy=$myproxy
 
 
 function checkurl() {
@@ -89,7 +137,6 @@ function readlink() {
 	GetStuff=`$lynx -hiddenlinks=ignore -dump -unique_urls -listonly $url`
 	for stuff in $GetStuff
 	do
-		#stuffreg='((^[0-9\.]+$)|(References))'
 		stuffreg='^[0-9\.]+$'
 		if  ! [[ $stuff =~ $stuffreg ]]
 		then
@@ -176,7 +223,10 @@ function webmenu() {
 	fi
 }
 
-# Sanitize the callsign but not necessarily verify it (its only for logging).
+# Trim whitespaces and newlines from the callsign but not necessarily verify it is sane (its only for logging).
+# I should probably do full sanitization here, although the callsign is passed from BPQ itself automatically
+# if you decide to run this script and expose direct access to it via telnet one could put any garbage in..
+
 callsign=${callsignin//[$'\t\r\n']} && callsignin=${callsignin%%*( )}
 
 echo "Welcome to Simple Packet Web"
