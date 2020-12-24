8e686ca4024b1cca838e87ce010ce76eceb71916
Red Tuby <pe1rrr@amsat.org>
2020-12-24 02:15:34 +0100

    bug fixes

--- inetd-services/portnoise.sh
+++ inetd-services/portnoise.sh
@@ -27,20 +27,18 @@ Ports[12]="AX/IP"
 # Globals
 QuitRegex='^(Q|q|B|b)$'
 MenuRegex='^(M|m)$'
-Width="60"
-Height="20"
+Width="79"
+Height="30"
 
 function PortMenu() {
 	echo "...it thickens!"
-	echo "iNotice: This tool is currently completely meaningless as I am still working on it"
-	echo "         Some charts may show a warning & are temporarily empty. That's all your fault."
 	echo ""
 	echo "Welcome, please choose a port:"
 	echo "[2]  APRS 144.800 Mhz"
 	echo "[3]  HF /Net105/ 14.102 USB @ 1300hz Center"
-	echo "[4]  HF BBS Fwd 14.102 USB @ 1900hz Center"
-	echo "[5]  HF QRP (interchanged w/Port 3) /Net105/ 14.102 USB @ 1300hz Center"
-	echo "[6]  HF QRP (interchanged w/Port 4) BBS Fwd 14.102 USB @ 1900hz Center"
+	echo "[4]  HF BBS Fwd 14.102 USB @ 2000hz Center"
+	#echo "[5]  HF QRP (interchanged w/Port 3) /Net105/ 14.102 USB @ 1300hz Center"
+	#echo "[6]  HF QRP (interchanged w/Port 4) BBS Fwd 14.102 USB @ 1900hz Center"
 	echo "[10] Telnet Interface"
 	echo "[11] Internet AXUDP Links"
 	echo "[12] Internet AXIP Proto 93 Links"
@@ -94,21 +92,35 @@ function AsciiPort() {
 	#
 	PortName=${Ports[$Port]}
 	# Export Data
-	rrdtool fetch --end now --start end-6h ${RRDDir}/port${Port}.rrd AVERAGE | tail -n +3 | awk -F ' ' '{print $1,$2}'  | grep -v 'nan' | sed 's/^/"/g;s/:/"/g' > ${TMPDir}/${Tag}_Out.csv 
-	rrdtool fetch --end now --start end-6h ${RRDDir}/port${Port}.rrd AVERAGE | tail -n +3 | awk -F ' ' '{print $1,$3}'  | grep -v 'nan' | sed 's/^/"/g;s/:/"/g' > ${TMPDir}/${Tag}_In.csv 
+	rrdtool fetch --end now --start end-24h ${RRDDir}/port${Port}.rrd AVERAGE | tail -n +3 | awk -F ' ' '{print $1,$2}'  | grep -v 'nan' | sed 's/^/"/g;s/:/"/g' > ${TMPDir}/${Tag}_Out.csv 
+	rrdtool fetch --end now --start end-24h ${RRDDir}/port${Port}.rrd AVERAGE | tail -n +3 | awk -F ' ' '{print $1,$3}'  | grep -v 'nan' | sed 's/^/"/g;s/:/"/g' > ${TMPDir}/${Tag}_In.csv 
 
 	# Smush data
 	paste <(for i in {1..$(wc -l "${TMPDir}/${Tag}_Out.csv" | cut -d' ' -f1)}; do echo $i; done) <(cut -d' ' -f 2 "${TMPDir}/${Tag}_Out.csv") > "${TMPDir}/${Tag}2_Out.csv"
+
 	paste <(for i in {1..$(wc -l "${TMPDir}/${Tag}_In.csv" | cut -d' ' -f1)}; do echo $i; done) <(cut -d' ' -f 2 "${TMPDir}/${Tag}_In.csv") > "${TMPDir}/${Tag}2_In.csv"
 
+	paste <(for j in $(cut -f1 ${TMPDir}/${Tag}_In.csv -d' ' | sed -e 's/"//g'); do date --date="@$j" +%H:%M; done) <(cut -d' ' -f2 "${TMPDir}/${Tag}_In.csv")  > "${TMPDir}/${Tag}_In.csv"
+	paste <(for k in $(cut -f1 ${TMPDir}/${Tag}_Out.csv -d' ' | sed -e 's/"//g'); do date --date="@$k" +%H:%M; done) <(cut -d' ' -f2 "${TMPDir}/${Tag}_Out.csv")  > "${TMPDir}/${Tag}_In.csv"
+
+
 	# Plot Graph
-	gnuplot <<< "set terminal dumb ${Width} ${Height}; plot \"${TMPDir}/${Tag}_Out.csv\" using 2:xticlabels(1) notitle with lines" | head -n ${CutCrap} 
+	gnuplot <<< "set terminal dumb ${Width} ${Height};  set ytics border nomirror out ; set xtics border nomirror out ;plot \"${TMPDir}/${Tag}_Out.csv\" using 2:xticlabels(1) notitle with lines" | head -n ${CutCrap} 
 	echo "    Port ${Port} ${PortName} - Inbound Traffic"
-	gnuplot <<< "set terminal dumb ${Width} ${Height}; plot \"${TMPDir}/${Tag}_In.csv\" using 2:xticlabels(1) notitle with lines" | head -n ${CutCrap} 
+
+	echo "Next Graph? (Y/n)"
+	read AskThem
+	AskThemClean=${AskThem//[$'\t\r\n']} && AskThem=${AskThem%%*( )}
+	if [[ $AskThemClean =~ (N|n) ]]
+	then
+		PortMenu
+	else
+		gnuplot <<< "set terminal dumb ${Width} ${Height};  set ytics border nomirror out ; set xtics border nomirror out ;plot \"${TMPDir}/${Tag}_In.csv\" using 2:xticlabels(1) notitle with lines" | head -n ${CutCrap} 
 	echo "    Port ${Port} ${PortName} - Outbound Traffic"
+	fi	
 	echo ""
 	echo "Legend has it that the Vertical Axis is adaptive Bytes/s"
-	echo "The Horizontal Axis shows the last 6 hours of activity"
+	echo "The Horizontal Axis shows the last 24 hours of activity"
 
 	# Clean up temp files
 	rm "${TMPDir}/${Tag}_In.csv" "${TMPDir}/${Tag}2_In.csv" "${TMPDir}/${Tag}2_Out.csv" "${TMPDir}/${Tag}_Out.csv"
